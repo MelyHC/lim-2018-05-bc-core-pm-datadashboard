e31bbf95d6820d70fd4ce94f6ab389d6b2940f0d
CrisEscobar <crisescobar0@hotmail.com>
2018-06-15 14:53:19 -0500

    demo 1

--- src/main.js
+++ src/main.js
@@ -1,19 +1,17 @@
 function mostrar(){
-document.getElementById("texto").style.display = 'none';    //esto sirve para que GENERAL SEA CLICKEABLE
-}
-const gen=document.getElementById('general'); //VARIABLE EVENTO CLICK GENERAL
-const al=document.getElementById('alumnas');  // VARIABLE EVENTO CLICK ALUMNA
-
-gen.addEventListener("click",() => {
-    document.getElementById("GENERAL").style.display = 'block';  
+    document.getElementById("seccion-alumnas").style.display = 'none';    //esto sirve para que GENERAL SEA CLICKEABLE
+    }
+    const gen=document.getElementById('general'); //VARIABLE EVENTO CLICK GENERAL
+    const al=document.getElementById('alumnas');  // VARIABLE EVENTO CLICK ALUMNA
     
-    mostrar();
-});
-
-al.addEventListener("click",() => {
-document.getElementById("texto").style.display = 'block';    
-document.getElementById("GENERAL").style.display = 'none';
-
-
-});
-
+    gen.addEventListener("click",() => {
+       document.getElementById("seccion-general").style.display = 'block'; 
+      
+       mostrar();
+    });
+    
+    al.addEventListener("click",() => {
+    document.getElementById("seccion-alumnas").style.display = 'block';   
+    document.getElementById("seccion-general").style.display = 'none';
+    });
+    
