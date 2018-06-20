764a2286cc38a5bdaf850cfbb113f91835f05599
CrisEscobar <crisescobar0@hotmail.com>
2018-06-19 20:54:37 -0500

    agregando funcion a inicio y radius a la tabla

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+//Funcion display para index.html
 function mostrar(){
     document.getElementById("seccion-alumnas").style.display = 'none';    //esto sirve para que GENERAL SEA CLICKEABLE
     }
@@ -14,4 +15,24 @@ function mostrar(){
     document.getElementById("seccion-alumnas").style.display = 'block';   
     document.getElementById("seccion-general").style.display = 'none';
     });
+
+
+    //Función y condiciones para usuario y contraseña de inicio.html
+    function validar()
+    {
+    var usuario = document.getElementById("usuario").value;
+    var contraseña = document.getElementById("contraseña").value;
+
+    if(usuario== "alejandralima" && contraseña == "12345")
+    {
+        location.href ="http://127.0.0.1:5500/src/index.html";       //propiedad para direccionar a un link
+    }
+    else
+    {
+        alert("Usuario y/o contraseña inválidos. Por favor verifique sus datos.");
+    }
+    }
+
+
+
     
