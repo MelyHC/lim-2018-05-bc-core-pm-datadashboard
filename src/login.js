5fb5e8829b14f447c3681098cc566f5b78f3172c
MelyHC <mely.hidalgo.crespo@gmail.com>
2020-08-16 21:55:04 -0500

    Detalles nombres ids

--- src/login.js
+++ src/login.js
@@ -1,13 +1,13 @@
 const user = document.getElementById('user');
 const password = document.getElementById('password');
-const formLogin = document.getElementById('formLogin');
+const formLogin = document.getElementById('form-login');
 
 //Función y condiciones para usuario y contraseña de inicio.html
 const validate = () => {
-  if (user.value == 'alejandra@laboratoria.la' && password.value == 'laboratorialima') {
+  if (user.value == 'alejandra@laboratoria.la' && password.value == 'laboratoria') {
     location.href = '../src/index.html';       //propiedad para direccionar a un link
   } else {
-    alert("Usuario y/o contraseña inválidos. Por favor verifique sus datos.");
+    alert('Usuario y/o contraseña inválidos. Por favor verifique sus datos.');
   };
 };
 
